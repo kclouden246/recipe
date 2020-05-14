af2aa2bbc2b19486e276f7998a597289120a957c
kclouden <kclouden@anyarinc.com>
2020-05-14 14:38:15 -0500

    Sync with github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
   state = {
-    isAddRecipeFormDisplayed : false
+    isAddRecipeFormDisplayed: false,
+    recipes: [],
+    newRecipeName: "",
+    newRecipeInstructions: ""
   }
-
+  
   toggleAddRecipeForm = () => {
     this.setState({isAddRecipeFormDisplayed: !this.state.isAddRecipeFormDisplayed})
   }
 
-  submitRecipe = () => {}
+  handleChange = (event) => {
+    const target = event.target;
+    const name = target.name;
+    
+    this.setState({[name]: target.value});
+  }
+  
+  submitRecipe = (event) => {
+    event.preventDefault()
+    this.setState({recipes: [
+      {
+        name: this.state.newRecipeName, 
+        instructions: this.state.newRecipeInstructions
+      }
+    ]})
+  }
 
   render(){
     const addNewRecipeForm = (
-      <form id="recipe-form"> 
+      <form id="recipe-form" onSubmit={this.submitRecipe}>
         <label htmlFor="newRecipeName">Recipe name: </label>
-        <input type="text" name="newRecipeName" />
+        <input type="text" name="newRecipeName" onChange={this.handleChange} value={this.state.newRecipeName} />
         <label htmlFor="newRecipeInstructions">Instructions:</label>
-        <textarea name="newRecipeInstructions" placeholder="write recipe instructions here..." />
+        <textarea name="newRecipeInstructions" 
+                  placeholder="write recipe instructions here..." 
+                  onChange={this.handleChange} 
+                  value={this.state.newRecipeInstructions} />
         <input type="submit" />
       </form>
     )
@@ -32,7 +52,14 @@ class App extends React.Component {
           ? addNewRecipeForm 
           : <button id="add-recipe" onClick={this.toggleAddRecipeForm}>Add Recipe</button>
         }
-        <p>There are no recipes to list.</p>
+        {
+          this.state.recipes.length > 0 ?
+          <ul>
+            <li>{ this.state.recipes[0].name }</li>
+          </ul> :
+          <p>There are no recipes to list.</p>
+        }
+        
       </div>
     )
   }
